67dad88e5aafcb5664a6aab328be048247ca1c13
Kousik S <kousik.s@bytebandits.in>
2025-11-10 11:25:28 +0530

    Update HeaderWrapper.tsx

--- src/components/common/HeaderWrapper.tsx
+++ src/components/common/HeaderWrapper.tsx
@@ -8,7 +8,7 @@ export default function HeaderWrapper({ children }: { children: ReactNode }) {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 4500);
+    const timer = setTimeout(() => setLoading(false), 4800);
     return () => clearTimeout(timer);
   }, []);
 
