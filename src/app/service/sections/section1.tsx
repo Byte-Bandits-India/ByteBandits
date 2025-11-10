da525337d2ddd7089712a08bc78ad59c4238dfc6
abraham-r-bytebandits <abraham.r@bytebandits.in>
2025-11-10 12:10:38 +0530

    correct the UI

--- src/app/service/sections/section1.tsx
+++ src/app/service/sections/section1.tsx
@@ -2,7 +2,7 @@
 export default function HeroSection() {
     return (
         <>
-            <div className="overflow-x-hidden bg-white h-screen text-center flex items-center justify-center">
+            <div className="overflow-x-hidden bg-white h-screen text-center flex items-center justify-center px-4">
                 <h1 className="text-[clamp(20px,6vw,67px)] font-medium leading-[1.1] font-inter max-w-[959px]">We revolutionize how businesses build, scale, and connect through technology.</h1>
             </div>
         </>
