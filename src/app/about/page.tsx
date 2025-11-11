64b800367c03123e574e1789bc677235f484934e
Kousik S <kousik.s@bytebandits.in>
2025-11-11 19:14:29 +0530

    Delete src/app/about/page.tsx

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-        <>
-            <div className="overflow-x-hidden px-6">
-                <h1 className="text-white">text</h1>
-            </div>
-        </>
-    );
-}
